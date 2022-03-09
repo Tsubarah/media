da95b159b074328a70dc3b75d30a2d2f8077d07e
Tsubarah <Tsubarah@hotmail.se>
2022-03-09 15:31:37 +0100

    minor change

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,6 @@ const auth = require('../middlewares/auth');
 const user_validationRules = require('../validation/user');
 const user_controller = require('../controllers/user_controller');
 
-// Test to get the GET
-router.get('/', (req, res, next) => {
-  res.send({ 
-    success: true,
-    data: {
-      msg: 'You called for me!'
-    }
-  });
-});
-
 router.use('/albums', auth.basic, require('./albums'));
 router.use('/photos', auth.basic, require('./photos'));
 
