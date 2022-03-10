5ad7331017ee9eeb8210c9f375eb927dfa7a90ec
Tsubarah <Tsubarah@hotmail.se>
2022-03-10 17:43:33 +0100

    added to json

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const knex = require('knex')({
 		host: process.env.DB_HOST || 'localhost',
 		port: process.env.DB_PORT || 3306,
 		charset: process.env.DB_CHARSET || 'utf8mb4',
-		database: process.env.DB_NAME || 'media',
+		database: process.env.DB_NAME || 'photo_api',
 		user: process.env.DB_USER || 'root',
 		password: process.env.DB_PASSWORD || '',
 	}
