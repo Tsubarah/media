2c4ee2bf0d41af610c4a1faf8a7d9f6771ef4af4
Tsubarah <Tsubarah@hotmail.se>
2022-03-10 20:57:40 +0100

    installed bcryptjs instead

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const debug = require('debug')('media:user_controller');
 const { matchedData, validationResult } = require('express-validator');
 const models = require('../models');
